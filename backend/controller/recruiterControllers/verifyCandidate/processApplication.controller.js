3efd3d62b206e19a3a26a2e210f10833b131ee2e
Krutik Gami <krutikgami354@gmail.com>
2025-08-18 17:19:03 +0530

    public job processApplication Controller added with its routes

---
+++ backend/controller/recruiterControllers/verifyCandidate/processApplication.controller.js
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+const processApplication = async(req,res)=>{
+    try {
+        const {jobId} = req.body;
+        const findJob = await prisma.job.findUnique({
+            where :{
+                id : jobId
+            }
+        })
+
+        if(!findJob){
+            return res.status(400).json({
+                message : "Job not found",
+                success : false
+            })
+        }
+
+        if(findJob.isPublicApplicationProcessed){
+            return res.status(400).json({
+                message : "Public applications are already processed",
+                success : false
+            })
+        }
+
+        const updateApplicationProcessed = await prisma.job.updateMany({
+            where:{
+                id : jobId
+            },
+            data:{
+                isPublicApplicationProcessed : false
+            }
+        })
+
+        if(!updateApplicationProcessed){
+            return res.status(400).json({
+                message : "Error updating job",
+                success : false
+            })
+        }
+
+        return res.status(200).json({
+            message : "Job application processed successfully",
+            success : true
+        })
+
+    } catch (error) {
+        console.log("Error Processing Application", error.message);
+        return res.status(500).json({
+            message : "Internal server error",
+            success : false,
+            error : error.message
+        })
+    }
+}
+
+export default processApplication;
--- backend/routes/recruiterRoutes/getAllAppliedCandidate.routes.js
+++ backend/routes/recruiterRoutes/getAllAppliedCandidate.routes.js
@@ -1,9 +1,12 @@
 import {Router} from 'express'
 import getAllAppliedCandidate from '../../controller/recruiterControllers/verifyCandidate/getAllAppliedCandidate.controller.js';
 import acceptRejectCandidate from '../../controller/recruiterControllers/verifyCandidate/acceptReject.controller.js';
+import processApplication from '../../controller/recruiterControllers/verifyCandidate/processApplication.controller.js';
+
 const router = Router();
 
 router.post('/getAllAppliedCandidates', getAllAppliedCandidate)
 router.post('/acceptReject', acceptRejectCandidate);
+router.post('/processApplication', processApplication);
 
 export default router;
